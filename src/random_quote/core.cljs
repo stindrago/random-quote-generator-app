ebc62b3bcc28dafa33d60cbadfa63f2de7ea2520
Costin Dragoi <costin@dragoi.me>
2020-09-16 21:55:07 +0200

    Add code

--- src/random_quote/core.cljs
+++ src/random_quote/core.cljs
@@ -1,17 +1,41 @@
 (ns random-quote.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [ajax.core :refer [GET]]
+   [reagent.core :as reagent :refer [atom]]))
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Quote!"
+                          :author "Costin Dragoi"}))
 
-(defn hello-world []
+(defn fetch-link! [state]
+  (GET "https://quote-garden.herokuapp.com/api/v2/quotes/random"
+       {:handler (fn [data]
+                   (println (get-in data ["quote" "quoteAuthor"]))
+
+                   (swap! state assoc :text (get-in data ["quote" "quoteText"]))
+                   (swap! state assoc :author (get-in data ["quote" "quoteAuthor"])))
+        :error-handler (fn [{:keys [status status-text]}]
+                         (js/console.log status status-text))}))
+
+(defn tweet-link [state]
+  (str "https://twitter.com/intent/tweet?hashtags=quote&text="
+       (:text @state) " - " (:author @state)))
+
+(defn quote [state]
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   [:h1 "Random Quote Generator"]
+   [:div.quote
+    [:div.quote-container
+     [:p.quote-content (:text @state)]
+     [:p.quote-author (:author @state)]]
+    [:div.quote-buttons
+     [:a {:href (tweet-link state)
+          :target "_blank"} "tweet"]
+     [:button {:on-click #(fetch-link! state)} "next quote"]]]]
+  )
 
 (defn start []
-  (reagent/render-component [hello-world]
+  (reagent/render-component [quote app-state]
                             (. js/document (getElementById "app"))))
 
 (defn ^:export init []
